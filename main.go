4dffc8325a8c14f347d05252cce3ab56dfab8520
cpmachado <cpmachado@protonmail.com>
2025-03-27 23:50:25 +0000

    Test with simpler timer

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 	"log/slog"
-	"math/rand"
-	"slices"
 	"time"
 )
 
 // A timing decorator
 var timingDecorator = Cravat{
-	Before: func() any {
-		return time.Now()
+	Before: func() interface{} {
+		v := time.Now()
+		return v
 	},
 	After: func(start interface{}) {
 		rStart := start.(time.Time)
@@ -20,26 +19,31 @@ var timingDecorator = Cravat{
 	},
 }
 
-// GenAndSortBigArrayOfInt generates and retrieves a sorted array of random
-// numbers, of size n between [0, n)
-func GenAndSortBigArrayOfInt(n int) []int {
+// SliceWithSleep creates slice of n+1 elements from [0, n]
+func SliceWithSleep(n int) []int {
 	result := []int{}
 	for len(result) < n {
-		result = append(result, rand.Intn(n))
+		result = append(result, len(result))
 	}
-
-	slices.Sort(result)
-	return result
+	return append(result, n)
 }
 
 func main() {
-	decoratedGenAndSortBigArrayOfInt, ok := PutCravat(timingDecorator, GenAndSortBigArrayOfInt)
+	decoratedGenAndSortBigArrayOfInt, ok := SimplerTimer(SliceWithSleep)
+	if !ok {
+		slog.Error("main/decorator", slog.String("error", "Failed to set decorator"))
+	}
+	// Call the decorated function
+	res := decoratedGenAndSortBigArrayOfInt(2025)
+	fmt.Println(len(res))
+
+	decoratedGenAndSortBigArrayOfInt, ok = PutCravat(timingDecorator, SliceWithSleep)
 
 	if !ok {
 		slog.Error("main/decorator", slog.String("error", "Failed to set decorator"))
 	}
 
 	// Call the decorated function
-	result := decoratedGenAndSortBigArrayOfInt(2025)
-	fmt.Println("Result:", result)
+	res = decoratedGenAndSortBigArrayOfInt(2025)
+	fmt.Println(len(res))
 }
---
+++ simplerTimer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+func SimplerTimer[T interface{}](fn T) (T, bool) {
+	fnValue, fnType := reflect.ValueOf(fn), reflect.TypeOf(fn)
+	if fnType.Kind() != reflect.Func {
+		return fn, false
+	}
+	return (reflect.MakeFunc(fnType, func(args []reflect.Value) []reflect.Value {
+		start := time.Now()
+		result := fnValue.Call(args)
+		duration := time.Since(start)
+		fmt.Printf("it took %v\n", duration)
+		return []reflect.Value(result)
+	}).Interface().(T)), true
+}
